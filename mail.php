0bfa423d86ceb72342505617f2704aef521baaf2
Drew Mendenhall <drew@mendenhall.io>
2015-07-26 14:20:40 -0500

    Convert Indentation to 2 spaces Fix Indentation Add editorconfig to enforce whitespace conformity html5 doctype

--- mail.php
+++ mail.php
@@ -8,11 +8,11 @@ header("Location: $referer");
 define('SUBJECT', 'Contact Form');
 function sendEmail($to, $from, $subj, $body)
 {
-	$date = date( 'r' );
-	$phpversion = phpversion();
-	$boundary = md5( time() );
-	$headers = "From: $from\n"."Date: $date\n"."Content-Type: text/html; charset=\"UTF-8\"\n";
-	mail(trim($to), trim($subj), $body, $headers );
+  $date = date( 'r' );
+  $phpversion = phpversion();
+  $boundary = md5( time() );
+  $headers = "From: $from\n"."Date: $date\n"."Content-Type: text/html; charset=\"UTF-8\"\n";
+  mail(trim($to), trim($subj), $body, $headers );
 }
 sendEmail(TO, trim($_POST['email']), SUBJECT, 'E-Mail from: '.$_POST['name'].'<br/><br/>Message: '.nl2br($_POST['message']));
 
