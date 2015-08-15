5978f957be00d17878d8e708e48b75b02ab94628
Drew Mendenhall <drew@mendenhall.io>
2015-08-15 15:32:46 -0500

    run dev server temporarily

--- scripts/starter.sh
+++ scripts/starter.sh
@@ -3,5 +3,7 @@
 if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
 then
   export PATH=/usr/local/bin:$PATH
-  forever start --sourceDir /etc/nginx/sites-availaable/gailkirklandphotography.com server.js >> /path/to/log.txt 2>&1
+  # forever start --sourceDir /etc/nginx/sites-availaable/gailkirklandphotography.com server.js >> /path/to/log.txt 2>&1
+  cd /etc/nginx/sites-availaable/gailkirklandphotography.com
+  npm start
 fi
