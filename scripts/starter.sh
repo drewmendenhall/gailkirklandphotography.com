7295f392aa2e26b7cefa0a7d57aa06b378cf838e
Drew Mendenhall <drew@mendenhall.io>
2015-08-15 15:33:42 -0500

    fix directory name in start-up script

--- scripts/starter.sh
+++ scripts/starter.sh
@@ -4,6 +4,6 @@ if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "
 then
   export PATH=/usr/local/bin:$PATH
   # forever start --sourceDir /etc/nginx/sites-availaable/gailkirklandphotography.com server.js >> /path/to/log.txt 2>&1
-  cd /etc/nginx/sites-availaable/gailkirklandphotography.com
+  cd /etc/nginx/sites-available/gailkirklandphotography.com
   npm start
 fi
