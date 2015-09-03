29ff494fd2c23cb7e9fae1f54b192090ee3ff1f7
Drew Mendenhall <drew@mendenhall.io>
2015-09-03 17:44:49 -0500

    fix error in twilight component

--- components/galleries/Twilight.jsx
+++ components/galleries/Twilight.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Carousel from './Carousel'
+import Carousel from '../Carousel'
 
 var pictureNames = [1, 2, 3]
 
@@ -13,7 +13,7 @@ export default class Twilight extends React.Component {
       >
         {pictureNames.map((name, index) => (
           <img src={`/images/twilight/${name}.jpg`} key={index} />
-        )}
+        ))}
       </Carousel>
     )
   }
