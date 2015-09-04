a3b382098fc7a91da4b90418513cde7cde261482
Drew Mendenhall <drew@mendenhall.io>
2015-09-03 20:13:33 -0500

    make email a link remove contact form

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -6,15 +6,8 @@ export default class Contact extends React.Component {
   render() {
     return (
       <div>
-        <p>gmkirkland@juno.com</p>
+        <p><a href="mailto:gmkirkland@juno.com">gmkirkland@juno.com</a></p>
         <p>214-538-9427</p>
-        <form method="post">
-          <input name="name" autoFocus placeholder="Name" />
-          <input name="email" placeholder="Email" />
-          <textarea name="message" placeholder="Message"></textarea>
-          <button type="reset">Clear Form</button>
-          <button className="button-primary">Send</button>
-        </form>
       </div>
     )
   }
