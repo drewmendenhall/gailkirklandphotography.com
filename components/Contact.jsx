4c5f54edb6410af441b8946e7b0812e70a3ffd04
Drew Mendenhall <drew@mendenhall.io>
2015-09-08 13:36:50 -0500

    remove form from contact page make email a `mailto:` link

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -6,8 +6,18 @@ export default class Contact extends React.Component {
   render() {
     return (
       <div>
-        <p><a href="mailto:gmkirkland@juno.com">gmkirkland@juno.com</a></p>
+        <p>You can reach us via phone or email at:</p>
         <p>214-538-9427</p>
+        <p><a href="gmkirkland@juno.com">gmkirkland@juno.com</a></p>
+        {/*
+          <form method="post">
+            <input name="name" autoFocus placeholder="Name" />
+            <input name="email" placeholder="Email" />
+            <textarea name="message" placeholder="Message"></textarea>
+            <button type="reset">Clear Form</button>
+            <button className="button-primary">Send</button>
+          </form>
+        */}
       </div>
     )
   }
