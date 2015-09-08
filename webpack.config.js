6c98044ecb75d3cd1733df48a08f46c2e4fe0312
Drew Mendenhall <drew@mendenhall.io>
2015-09-08 17:20:20 -0500

    fix `publicPath` in webpack config to match production build path

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,7 @@ module.exports = {
   //   ],
   // },
   output: {
-    // path: path.join(__dirname, 'public'),
-    path: __dirname,
+    path: path.join(__dirname, 'public'),
     publicPath: '/',
     filename: 'bundle.js',
   },
