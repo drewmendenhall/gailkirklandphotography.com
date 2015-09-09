7b7dcd27f3feb42273557e843a6fa135dece4eb8
Drew Mendenhall <drew@mendenhall.io>
2015-09-08 21:39:52 -0500

    use `/root` instead of `~`

--- scripts/starter.sh
+++ scripts/starter.sh
@@ -2,7 +2,7 @@
 
 if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
 then
-  export NVM_DIR="~/.nvm"
+  export NVM_DIR="/root/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
   cd /etc/nginx/sites-available/gailkirklandphotography.com
   NODE_ENV=production npm start >> /var/log/node/gailkirklandphotography.com/log 2>&1
