976d140adbcf0ae936c4e0b4b8ec58c8fd6a63ba
Drew Mendenhall <drew@mendenhall.io>
2015-09-16 06:00:22 -0500

    make top nav usable on mobile phones use hamburger menu design pattern

--- components/TopNav.jsx
+++ components/TopNav.jsx
@@ -7,31 +7,39 @@ import {Link} from 'react-router'
 export default class TopNav extends React.Component {
   render() {
     return (
-      <nav className="top-nav">
-        <ul>
-          <li><Link to="home" className="top-nav-link">home</Link></li>
-          <li><Link to="about" className="top-nav-link">about</Link></li>
-          <li>
-            <Link to="galleries" className="top-nav-link">galleries</Link>
-            <ul>
-              <li><Link to="/galleries/dogs" className="top-nav-link">dogs</Link></li>
-              <li><Link to="/galleries/horses" className="top-nav-link">horses</Link></li>
-              <li><Link to="/galleries/people" className="top-nav-link">people</Link></li>
-              <li><Link to="/galleries/twilight" className="top-nav-link">twilight</Link></li>
-            </ul>
-          </li>
-          <li>
-            <Link to="sessions" className="top-nav-link">sessions</Link>
-            <ul>
-              <li><Link to="/sessions/cats" className="top-nav-link">cats</Link></li>
-              <li><Link to="/sessions/dogs" className="top-nav-link">dogs</Link></li>
-              <li><Link to="/sessions/horses" className="top-nav-link">horses</Link></li>
-              <li><Link to="/sessions/twilight" className="top-nav-link">twilight</Link></li>
-            </ul>
-          </li>
-          <li><Link to="contact" className="top-nav-link">contact us</Link></li>
-        </ul>
-      </nav>
+      <label className="top-nav-control-label">
+        <i className="top-nav-control-icon"></i>
+        <input
+          className="top-nav-control"
+          type="checkbox"
+        />
+        <div className="top-nav-control-overlay"></div>
+        <nav className="top-nav">
+          <ul>
+            <li><Link to="home" className="top-nav-link">home</Link></li>
+            <li><Link to="about" className="top-nav-link">about</Link></li>
+            <li>
+              <Link to="galleries" className="top-nav-link">galleries</Link>
+              <ul>
+                <li><Link to="/galleries/dogs" className="top-nav-link">dogs</Link></li>
+                <li><Link to="/galleries/horses" className="top-nav-link">horses</Link></li>
+                <li><Link to="/galleries/people" className="top-nav-link">people</Link></li>
+                <li><Link to="/galleries/twilight" className="top-nav-link">twilight</Link></li>
+              </ul>
+            </li>
+            <li>
+              <Link to="sessions" className="top-nav-link">sessions</Link>
+              <ul>
+                <li><Link to="/sessions/cats" className="top-nav-link">cats</Link></li>
+                <li><Link to="/sessions/dogs" className="top-nav-link">dogs</Link></li>
+                <li><Link to="/sessions/horses" className="top-nav-link">horses</Link></li>
+                <li><Link to="/sessions/twilight" className="top-nav-link">twilight</Link></li>
+              </ul>
+            </li>
+            <li><Link to="contact" className="top-nav-link">contact us</Link></li>
+          </ul>
+        </nav>
+      </label>
     )
   }
 }
