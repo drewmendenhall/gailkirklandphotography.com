07e8330a9ed62af8c8cf4c1544540f20827554b9
Drew Mendenhall <drew@mendenhall.io>
2015-09-16 07:14:26 -0500

    `git checkout` on start

--- scripts/starter.sh
+++ scripts/starter.sh
@@ -2,6 +2,7 @@
 
 if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
 then
+  GIT_WORK_TREE=/etc/nginx/sites-available/gailkirklandphotography.com git checkout -f
   export NODE_ENV=production
   export NVM_DIR="/root/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
