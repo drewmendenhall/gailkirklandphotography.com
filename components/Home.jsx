1cb6dac2b5b36a8fd5fc1712edae3d82946d92a2
Drew Mendenhall <drew@mendenhall.io>
2015-10-06 03:45:43 -0500

    make `Home` use `Gallery`

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,25 +1,14 @@
 import React from 'react'
-// import ImageGallery from 'react-image-gallery'
-// import ImageGallery from 'react-image-gallery/src/ImageGallery.react'
 
-import Carousel from './Carousel'
-
-var pictureNames = []
-for (var i = 1; i <= 20; i++) {
-  pictureNames.push(i)
-}
+import Gallery from './Gallery'
 
 export default class Home extends React.Component {
   render() {
     return (
-      <Carousel
-        autoplay
-        slideInterval={3000}
-      >
-        {pictureNames.map((name, index) => (
-          <img src={`/images/home/${name}.jpg`} key={index} />
-        ))}
-      </Carousel>
+      <Gallery
+        {...this.props}
+        params={Object.assign({galleryId: 'home'}, this.props.params)}
+      />
     )
   }
 }
