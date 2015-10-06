5732ae1e9cf4483788f59bb0bc69a52ee3cdaeb8
Drew Mendenhall <drew@mendenhall.io>
2015-10-06 03:41:31 -0500

    add reminder to use `IndexRoute` when it's available in `react-router`

--- components/TopNav.jsx
+++ components/TopNav.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {Link} from 'react-router'
+import {IndexLink, Link} from 'react-router'
 
 // import '../styles/top-nav'
 import galleriesObject from '../public/galleries.json'
