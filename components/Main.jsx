b47b87feb6b79a2e8b1e880699127923bfb3043a
Drew Mendenhall <drew@mendenhall.io>
2015-10-11 04:28:40 -0500

    export `Main` as a function it's stateless

--- components/Main.jsx
+++ components/Main.jsx
@@ -9,59 +9,50 @@ import {
 
 import TopNav from './TopNav'
 
-export default class Main extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {}
-  }
-  render() {
-    return (
-      <main>
-        <Helmet
-          link={[
-            {rel: 'license', href: '/copyright'},
-          ]}
-          meta={[
-            {name: 'msapplication-TileColor', content: '#414141'},
-            {name: 'msapplication-TileImage', content: '/touch-icon-144x144.png'},
-            {name: 'viewport', content: 'width=device-width,initial-scale=1'},
-          ]}
-          titleTemplate="%s | Gail Kirkland Photography"
-        />
-        <header>
-          <IndexLink to="/" className="logo-link">
-            <img src="/images/logo-pink.svg" />
-            <span className="logo-company-name">
-              <span>Gail Kirkland</span>
-              <small>Photography</small>
-            </span>
-          </IndexLink>
-          <TopNav />
-        </header>
-        <div className="sub-header">
-          <span className="tagline">
-            A picture says more than a thousand words...
-          </span>
-          {/*
-          <Link to="/contact" className="book-session-button button-primary">
-            Book a Session
-          </Link>
-          */}
-        </div>
-        <section className="content">
-          {this.props.children}
-        </section>
-        <footer>
-          <div>
-            <Link to="/copyright" className="copyright" rel="license">
-              © Gail Kirkland Photography
-              {' '}
-              {new Date().getFullYear()}
-            </Link>
-          </div>
-        </footer>
-      </main>
-    )
-  }
-}
+export default ({children}) => (
+  <main>
+    <Helmet
+      link={[
+        {rel: 'license', href: '/copyright'},
+      ]}
+      meta={[
+        {name: 'msapplication-TileColor', content: '#414141'},
+        {name: 'msapplication-TileImage', content: '/touch-icon-144x144.png'},
+        {name: 'viewport', content: 'width=device-width,initial-scale=1'},
+      ]}
+      titleTemplate="%s | Gail Kirkland Photography"
+    />
+    <header>
+      <IndexLink to="/" className="logo-link">
+        <img src="/images/logo-pink.svg" />
+        <span className="logo-company-name">
+          <span>Gail Kirkland</span>
+          <small>Photography</small>
+        </span>
+      </IndexLink>
+      <TopNav />
+    </header>
+    <div className="sub-header">
+      <span className="tagline">
+        A picture says more than a thousand words...
+      </span>
+      {/*
+      <Link to="/contact" className="book-session-button button-primary">
+        Book a Session
+      </Link>
+      */}
+    </div>
+    <section className="content">
+      {children}
+    </section>
+    <footer>
+      <div>
+        <Link to="/copyright" className="copyright" rel="license">
+          © Gail Kirkland Photography
+          {' '}
+          {new Date().getFullYear()}
+        </Link>
+      </div>
+    </footer>
+  </main>
+)
