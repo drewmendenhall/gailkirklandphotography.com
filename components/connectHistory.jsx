e0749df6d46f0245945545729df3013a95422712
Drew Mendenhall <drew@mendenhall.io>
2015-10-12 13:26:03 -0500

    add `displayName` to `connectHistory`

--- components/connectHistory.jsx
+++ components/connectHistory.jsx
@@ -3,6 +3,7 @@ import {History} from 'react-router'
 
 export default function connectHistory(Component) {
   return React.createClass({
+    displayName: 'connectHistory',
     mixins: [History],
     render() {
       return <Component {...this.props} history={this.history} />
