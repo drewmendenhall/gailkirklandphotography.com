0c301153c3b2f3e820911ac4dd477d120f1f1711
Drew Mendenhall <drew@mendenhall.io>
2015-10-14 23:35:46 -0500

    move site to `/var/www` always execute `starter.sh`

--- scripts/starter.sh
+++ scripts/starter.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
-then
-  GIT_WORK_TREE=/etc/nginx/sites-available/gailkirklandphotography.com git checkout -f
-  export NODE_ENV=production
-  export NVM_DIR="/root/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-  cd /etc/nginx/sites-available/gailkirklandphotography.com
-  npm i
-  npm start >> /var/log/node/gailkirklandphotography.com/log 2>&1
-fi
+SITE_ROOT=/var/www/gailkirklandphotography.com
+
+GIT_WORK_TREE=$SITE_ROOT git checkout -f
+
+export NVM_DIR="/root/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+export NODE_ENV=production
+cd $SITE_ROOT
+npm i
+npm start >> /var/log/node/gailkirklandphotography.com 2>&1
