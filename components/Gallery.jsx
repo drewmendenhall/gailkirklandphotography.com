4a95b36dcf5a6b7e6eb2a5ce39f31e2f5fc6d42a
Drew Mendenhall <drew@mendenhall.io>
2015-11-06 01:44:28 -0600

    use stateless component for `Gallery`

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -5,48 +5,46 @@ import Carousel from './Carousel'
 
 import galleries from '../public/galleries.json'
 
-export default class Gallery extends React.Component {
-  render() {
-    const {location, params} = this.props
-    const {
-      galleryId,
-      pictureId,
-    } = params
-    const gallery = galleries[galleryId]
-    const pictures = gallery.pictures.map((picture) => {
-      picture.route = `/galleries/${gallery.id}/${picture.id}`
-      return picture
-    })
-    let index = pictureId && pictures.findIndex((picture) => picture.id === pictureId)
+export default (props) => {
+  const {location, params} = props
+  const {
+    galleryId,
+    pictureId,
+  } = params
+  const gallery = galleries[galleryId]
+  const pictures = gallery.pictures.map((picture) => {
+    picture.route = `/galleries/${gallery.id}/${picture.id}`
+    return picture
+  })
+  let index = pictureId && pictures.findIndex((picture) => picture.id === pictureId)
 
-    if (index === -1) index = 0
+  if (index === -1) index = 0
 
-    const picture = pictures[index]
+  const picture = pictures[index]
 
-    return (
-      <div>
-        {location.pathname !== '/' &&
-          <Helmet
-            title={`${(this.props.index ?
-              picture.title + ' | ' : ''
-            )}${gallery.title}`}
-          />
+  return (
+    <div>
+      {location.pathname !== '/' &&
+        <Helmet
+          title={`${(props.index ?
+            picture.title + ' | ' : ''
+          )}${gallery.title}`}
+        />
+      }
+      <Carousel
+        autoplay
+        slideInterval={3000}
+        items={pictures}
+        index={index}
+      >
+        {pictureId ?
+          <img src={picture.url} />
+        :
+          pictures.map((picture, index) => (
+            <img src={picture.url} key={index} />
+          ))
         }
-        <Carousel
-          autoplay
-          slideInterval={3000}
-          items={pictures}
-          index={index}
-        >
-          {pictureId ?
-            <img src={picture.url} />
-          :
-            pictures.map((picture, index) => (
-              <img src={picture.url} key={index} />
-            ))
-          }
-        </Carousel>
-      </div>
-    )
-  }
+      </Carousel>
+    </div>
+  )
 }
