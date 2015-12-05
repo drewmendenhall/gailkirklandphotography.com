6d0c93aadce30fda37c6cdf1bddcd973ef8ea9fe
Drew Mendenhall <drew@mendenhall.io>
2015-12-05 05:25:43 -0600

    remove redundant `Object.assign` polypill this is already covered by babel

--- client.js
+++ client.js
@@ -3,12 +3,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Router from 'react-router'
-import assign from 'object-assign'
 import createHistory from 'history/lib/createBrowserHistory'
 
 import routes from './routes'
-
-Object.assign = assign
 
 const history = createHistory()
 
