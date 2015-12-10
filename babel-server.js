5df47a9dd8c2e8199c6d58fc8c17ddf2207f6fe4
Drew Mendenhall <drew@mendenhall.io>
2015-12-09 22:52:50 -0600

    remove newrelic apm

--- babel-server.js
+++ babel-server.js
@@ -1,6 +1,3 @@
 require('babel-register')
 
-var PROD = (process.env.NODE_ENV === 'production')
-if (PROD) require('newrelic')
-
 require('./server')
--- newrelic.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  config: {
-    app_name: 'Gail Kirkland Photography',
-    license_key: process.env.NEWRELIC_LICENSE_KEY,
-    logging: {
-      level: 'info',
-    },
-  },
-}
