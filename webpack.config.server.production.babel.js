9a939270d8da8809663c7eaa312b290bfb79a71c
Drew Mendenhall <drew@mendenhall.io>
2016-02-19 00:26:46 -0600

    build production server via webpack run via vanilla node

---
+++ webpack.config.server.production.babel.js
@@ -0,0 +1,25 @@
+import nodeExternals from 'webpack-node-externals'
+import webpack from 'webpack'
+
+import config from './webpack.config.production.babel'
+
+export default ({
+  ...config,
+  entry: {
+    server: './server',
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    filename: '[name].js',
+  },
+
+  externals: [nodeExternals()],
+  node: {
+    __dirname: false,
+  },
+  target: 'node',
+
+  plugins: (config.plugins || []).filter((plugin) => (
+    !(plugin instanceof webpack.optimize.CommonsChunkPlugin)
+  )),
+})
