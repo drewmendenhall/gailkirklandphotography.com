b64831b2032d7cb579bb09e662d4283c44a00aef
Drew Mendenhall <drew@mendenhall.io>
2016-03-06 16:38:45 -0600

    whitespace correct indentation

--- webpack.config.development.babel.js
+++ webpack.config.development.babel.js
@@ -9,48 +9,48 @@ const hotEntry = ['webpack-hot-middleware/client'].concat(
 
 module.exports = ({
   ...config,
-    devtool: '#cheap-module-eval-source-map',
-    devServer: {
-      publicPath: config.output.publicPath,
-    },
-    entry: (typeof config.entry === 'string' ?
-      hotEntry
-    :
-      {
-        ...config.entry,
-        app: hotEntry,
-      }
-    ),
-    module: {
-      ...config.module,
-      loaders: config.module.loaders.map((loader) => {
-        if (loader.loader === 'babel') {
-          if (!loader.query) loader.query = {}
-          if (!Array.isArray(loader.query.plugins)) loader.query.plugins = []
-          let plugin = loader.query.plugins.find((plugin) => (
-            plugin[0] === 'react-transform'
-          ))
-          if (!plugin) {
-            plugin = ['react-transform', {}]
-            loader.query.plugins.push(plugin)
-          }
-          let pluginOptions = plugin[1]
-          if (!Array.isArray(pluginOptions.transforms)) {
-            pluginOptions.transforms = []
-          }
-
-          pluginOptions.transforms.push({
-            transform: 'react-transform-hmr',
-            imports: ['react'],
-            locals: ['module'],
-          })
+  devtool: '#cheap-module-eval-source-map',
+  devServer: {
+    publicPath: config.output.publicPath,
+  },
+  entry: (typeof config.entry === 'string' ?
+    hotEntry
+  :
+    {
+      ...config.entry,
+      app: hotEntry,
+    }
+  ),
+  module: {
+    ...config.module,
+    loaders: config.module.loaders.map((loader) => {
+      if (loader.loader === 'babel') {
+        if (!loader.query) loader.query = {}
+        if (!Array.isArray(loader.query.plugins)) loader.query.plugins = []
+        let plugin = loader.query.plugins.find((plugin) => (
+          plugin[0] === 'react-transform'
+        ))
+        if (!plugin) {
+          plugin = ['react-transform', {}]
+          loader.query.plugins.push(plugin)
+        }
+        let pluginOptions = plugin[1]
+        if (!Array.isArray(pluginOptions.transforms)) {
+          pluginOptions.transforms = []
         }
 
-        return loader
-      }),
-    },
-    plugins: (config.plugins || []).concat([
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.NoErrorsPlugin(),
-    ]),
+        pluginOptions.transforms.push({
+          transform: 'react-transform-hmr',
+          imports: ['react'],
+          locals: ['module'],
+        })
+      }
+
+      return loader
+    }),
+  },
+  plugins: (config.plugins || []).concat([
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+  ]),
 })
--- webpack.config.production.babel.js
+++ webpack.config.production.babel.js
@@ -4,19 +4,19 @@ import config from './webpack.config.babel'
 
 export default ({
   ...config,
-    plugins: [
-      ...(config.plugins || []),
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify('production'),
-        },
-      }),
+  plugins: [
+    ...(config.plugins || []),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
 
-      new webpack.optimize.DedupePlugin(),
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false,
-        },
-      }),
-    ],
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+    }),
+  ],
 })
