0c9c4e58ad5157df5e0d2a916609c34f7711a1ca
Drew Mendenhall <drew@mendenhall.io>
2016-03-10 23:36:58 -0600

    update contact page
    use correct class name

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 
 // import '../styles/contact'
 
-export default class About extends React.Component {
+export default class Contact extends React.Component {
   render() {
     return (
-      <form>
-        <input name="name" autoFocus placeholder="Name" />
-        <input name="email" placeholder="Email" />
-        <textarea name="message" placeholder="Message"></textarea>
-        <button type="reset">Clear Form</button>
-        <button className="button-primary">Send</button>
-      </form>
+      <div>
+        <p>gmkirkland@juno.com</p>
+        <p>214-538-9427</p>
+        <form method="post">
+          <input name="name" autoFocus placeholder="Name" />
+          <input name="email" placeholder="Email" />
+          <textarea name="message" placeholder="Message"></textarea>
+          <button type="reset">Clear Form</button>
+          <button className="button-primary">Send</button>
+        </form>
+      </div>
     )
   }
 }
