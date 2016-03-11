e7f52127ac600142d3d6bf4d0fb132e26be4c244
Drew Mendenhall <drew@mendenhall.io>
2016-03-10 23:22:10 -0600

    remove semicolon

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -73,7 +73,7 @@ export default class Carousel extends React.Component {
     if (this.props.autoplay &&
       prevProps.slideInterval !== this.props.slideInterval
     ) {
-      this.resetAutoplay();
+      this.resetAutoplay()
     }
   }
   componentWillUnmount() {
