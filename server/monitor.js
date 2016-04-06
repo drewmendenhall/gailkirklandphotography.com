1e213927d2518bf4625f7b15de2d38459638344b
Drew Mendenhall <drew@mendenhall.io>
2016-04-05 23:32:53 -0500

    passthrough `stdout` and `stderr`

--- server/monitor.js
+++ server/monitor.js
@@ -9,7 +9,11 @@ fs.watch(path.resolve(__dirname, 'server.js'), restartServer)
 function restartServer() {
   console.log(`${timestamp('[YYYY/MM/DD HH:mm:ss.ms]')} detected change`)
 
+  exec('npm prune && npm install', (error, stdout, stderr) => {
     if (error) throw error
+
+    console.log(stdout)
+    console.error(stderr)
 
     exec('npm run serve:prod:www:stop && npm run serve:prod:www')
   })
