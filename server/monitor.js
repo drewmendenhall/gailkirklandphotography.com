bc70fe4511cb5ead3ebfd768f98b57d5ae0f8e5c
Drew Mendenhall <drew@mendenhall.io>
2016-04-05 23:32:24 -0500

    log timestamped message when change is detected

--- server/monitor.js
+++ server/monitor.js
@@ -1,12 +1,14 @@
 import {exec} from 'child_process'
 import fs from 'fs'
 import path from 'path'
+import timestamp from 'time-stamp'
 
 fs.watch(path.resolve(__dirname, '../package.json'), restartServer)
 fs.watch(path.resolve(__dirname, 'server.js'), restartServer)
 
 function restartServer() {
-  exec('npm prune && npm install', (error) => {
+  console.log(`${timestamp('[YYYY/MM/DD HH:mm:ss.ms]')} detected change`)
+
     if (error) throw error
 
     exec('npm run serve:prod:www:stop && npm run serve:prod:www')
