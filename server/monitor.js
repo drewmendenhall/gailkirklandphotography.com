5865ea932c820436f3ed1bb932be466a08789406
Drew Mendenhall <drew@mendenhall.io>
2016-04-08 00:47:06 -0500

    only perform package changes for `package.json`

--- server/monitor.js
+++ server/monitor.js
@@ -8,27 +8,37 @@ const log = (...messages) => {
     timestamp('[YYYY/MM/DD HH:mm:ss.ms]'),
   ].concat(messages))
 }
+const serverPath = path.resolve(__dirname, 'server.js')
 
 chokidar.watch([
   path.resolve(__dirname, '../package.json'),
-  path.resolve(__dirname, 'server.js'),
-]).on('change', function restartServer(path) {
+  serverPath,
+]).on('change', (path) => {
   log(`${path} changed`)
-  log('restarting server')
 
-  exec('npm prune && npm install', (error, stdout, stderr) => {
-    if (error) throw error
-
-    console.log(stdout)
-    console.error(stderr)
-
-    exec('npm run serve:prod:www:stop && npm run serve:prod:www', (error, stdout, stderr) => {
+  if (path === serverPath) {
+    restartServer()
+  } else {
+    exec('npm prune && npm install', (error, stdout, stderr) => {
       if (error) throw error
 
       console.log(stdout)
       console.error(stderr)
+
+      restartServer()
     })
-  })
+  }
 })
+
+function restartServer() {
+  log('restarting server')
+
+  exec('npm run serve:prod:www:stop && npm run serve:prod:www', (error, stdout, stderr) => {
+    if (error) throw error
+
+    console.log(stdout)
+    console.error(stderr)
+  })
+}
 
 log('monitor started')
