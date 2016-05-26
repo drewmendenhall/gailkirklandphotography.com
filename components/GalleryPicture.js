685ec094b860d58e6c447a2a48d29d96017bc73d
Drew Mendenhall <drew@mendenhall.io>
2016-05-26 00:00:22 -0500

    collapse whitespace of gallery picture styles

--- components/GalleryPicture.js
+++ components/GalleryPicture.js
@@ -6,32 +6,28 @@ import Helmet from 'react-helmet'
 // TODO: use object rest to spread all props
 // export default ({picture, ...props}) => (
 export default ({id, picture}) => {
+  const {minHeight, minWidth} = picture
+
   id = id || `picture-${picture.id}`
 
   return (
     <picture id={id}>
       <Helmet
         style={[{cssText: picture.srcSet.map((src, index) => (`${index > 0 ? `
-          @media
-            (min-width: ${src.width}px) and
-            (min-height: ${src.height}px)
-          {
+          @media (min-width: ${src.width}px) and (min-height: ${src.height}px) {
         ` : ''}
             #${id} {
               background-image: url(${encodeURI(src.url)});
             }
           ${index > 0 ? '}' : ''}
-        `)).join('') + `
-          @media
-            (min-width: ${picture.minWidth}px) and
-            (min-height: ${picture.minHeight}px)
-          {
+        `)).join('').replace(/\n\s*/g, '') + `
+          @media (min-width: ${minWidth}px) and (min-height: ${minHeight}px) {
             #${id} {
               background-image: url(${encodeURI(picture.url)});
               background-size: auto;
             }
           }
-        `}]}
+        `.replace(/\n\s*/g, '')}]}
       />
       {picture.srcSet.slice(1).reverse().map((src) => (
         <source
