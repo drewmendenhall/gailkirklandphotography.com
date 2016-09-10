860484b16eb579279bed7c54fb6974c1aab4a053
Drew Mendenhall <drew@mendenhall.io>
2016-09-09 22:30:00 -0500

    Use `apt` instead of `apt-get`.
    `apt` is now recommended starting with Ubuntu 16.04

--- vagrant/docker.sh
+++ vagrant/docker.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-apt-get install -y linux-image-extra-$(uname -r) python-pip
+apt install -y linux-image-extra-$(uname -r) python-pip
 
 export DISTRO="$(lsb_release -s -c)"
 
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-$DISTRO main" | sudo tee /etc/apt/sources.list.d/docker.list
 
-apt-get update
-apt-get install -y docker-engine
+apt update
+apt install -y docker-engine
 
 pip install --upgrade pip
 pip install docker-compose
