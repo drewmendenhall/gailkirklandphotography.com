6f944b4fb2eb6db6120aa5ffef963b50d2b76d52
Drew Mendenhall <drew@mendenhall.io>
2019-06-09 15:00:54 -0500

    Remove bluebird.

--- gulp/images/resize-image.js
+++ gulp/images/resize-image.js
@@ -1,8 +1,8 @@
-const Promise = require('bluebird')
-
-const fs = Promise.promisifyAll(require('fs'))
-const im = Promise.promisifyAll(require('imagemagick'))
 const path = require('path')
+const {promisify} = require('util')
+
+const stat = promisify(require('fs').stat)
+const resize = promisify(require('imagemagick').resize)
 
 const {pictureWidths} = require('./layout')
 
@@ -10,15 +10,15 @@ export default (filename) => {
   const ext = path.extname(filename)
 
   return Promise.all([
-    fs.statAsync(filename),
-    (fs.statAsync(filename.replace(ext, `-${pictureWidths[0]}` + ext))
+    stat(filename),
+    (stat(filename.replace(ext, `-${pictureWidths[0]}` + ext))
       .catch(() => {})
     ),
   ]).then(([srcStat, dstStat]) => (
     !dstStat || srcStat.mtime > dstStat.mtime
   )).then((shouldWriteImages) => {
     return (shouldWriteImages && Promise.all(pictureWidths.map((width) => (
-      im.resizeAsync({
+      resize({
         srcPath: filename,
         dstPath: filename.replace(ext, `-${width}` + ext),
         width,
