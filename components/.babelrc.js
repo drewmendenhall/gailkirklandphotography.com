5a0fca879835b5849a390efeae0fcfb0c989816e
Drew Mendenhall <drew@mendenhall.io>
2019-06-14 00:57:43 -0500

    fixup! babel 7

--- components/.babelrc.js
+++ components/.babelrc.js
@@ -1,18 +1,16 @@
+const __DEV__ = process.env.NODE_ENV !== 'production'
+
 module.exports = {
   extends: '../.babelrc.js',
 
-  env: {
-    production: {
-      plugins: [
-        '@babel/transform-react-constant-elements',
-        '@babel/transform-react-inline-elements',
-      ],
-    },
-  },
   plugins: [
     ['@babel/proposal-class-properties', {loose: true}],
+    ...(__DEV__ ? [] : [
+      '@babel/transform-react-constant-elements',
+      '@babel/transform-react-inline-elements',
+    ]),
   ],
   presets: [
-    '@babel/react',
+    ['@babel/react', {development: __DEV__}],
   ],
 }
