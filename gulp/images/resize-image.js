9ed64bf111053004045381103c22443d4b28a814
Drew Mendenhall <drew@mendenhall.io>
2019-07-28 19:35:51 -0500

    Use async / await.

--- gulp/images/resize-image.js
+++ gulp/images/resize-image.js
@@ -6,26 +6,24 @@ const resize = promisify(require('imagemagick').resize)
 
 const {pictureWidths} = require('./layout')
 
-export default (filename) => {
+export default async (filename) => {
   const ext = path.extname(filename)
 
-  return Promise.all([
+  const [srcStat, dstStat] = await Promise.all([
     stat(filename),
-    (stat(filename.replace(ext, `-${pictureWidths[0]}` + ext))
-      .catch(() => {})
-    ),
-  ]).then(([srcStat, dstStat]) => (
-    !dstStat || srcStat.mtime > dstStat.mtime
-  )).then((shouldWriteImages) => {
-    return (shouldWriteImages && Promise.all(pictureWidths.map((width) => (
-      resize({
-        srcPath: filename,
-        dstPath: filename.replace(ext, `-${width}` + ext),
-        width,
-      }).then((stdout, stderr) => {
-        if (stdout) console.log(stdout)
-        if (stderr) console.error(stderr)
-      })
-    ))))
-  })
+    stat(filename.replace(ext, `-${pictureWidths[0]}` + ext)).catch(() => {}),
+  ])
+
+  const shouldWriteImages = !dstStat || srcStat.mtime > dstStat.mtime
+
+  return (shouldWriteImages && Promise.all(pictureWidths.map((width) => (
+    resize({
+      srcPath: filename,
+      dstPath: filename.replace(ext, `-${width}` + ext),
+      width,
+    }).then((stdout, stderr) => {
+      if (stdout) console.log(stdout)
+      if (stderr) console.error(stderr)
+    })
+  ))))
 }
