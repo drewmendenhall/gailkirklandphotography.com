b24055832f5a168241c9d3820858410c5a38f6c0
Drew Mendenhall <drew@mendenhall.io>
2019-09-21 03:44:41 -0500

    Use plugin error package and remove gulp utils.

--- gulp/images/index.js
+++ gulp/images/index.js
@@ -1,10 +1,10 @@
+import PluginError from 'plugin-error'
 import filter from 'gulp-filter'
 import gulp from 'gulp'
 import imagemin from 'gulp-imagemin'
 import livereload from 'gulp-livereload'
 import newer from 'gulp-newer'
 import path from 'path'
-import {log, PluginError} from 'gulp-util'
 import {Transform} from 'stream'
 
 import generateSrcsets from './generate-srcsets'
@@ -34,7 +34,7 @@ gulp.task('images', () =>
 
           const relativePath = path.join(destPath, file.relative)
 
-          log(`images generated for ${relativePath}`)
+          console.log(`images generated for ${relativePath}`)
 
           try {
             await resizeImage(relativePath)
