bc325840c2c87efe4fe2326fb74ffdea1ad5e02d
Drew Mendenhall <drew@mendenhall.io>
2019-09-21 11:27:51 -0500

    Format dotfiles.

--- .babelrc.js
+++ .babelrc.js
@@ -1,13 +1,9 @@
-const COMMONJS = (!process.env.WEBPACK_VERSION ||
-  /^1\./.test(process.env.WEBPACK_VERSION)
-)
+const COMMONJS =
+  !process.env.WEBPACK_VERSION || /^1\./.test(process.env.WEBPACK_VERSION)
 
 module.exports = {
   plugins: [
     '@babel/proposal-object-rest-spread',
     '@babel/transform-runtime',
-  ].concat(COMMONJS
-    ? '@babel/transform-modules-commonjs'
-    : []
-  )
+  ].concat(COMMONJS ? '@babel/transform-modules-commonjs' : []),
 }
